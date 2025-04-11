e75c2073c7aa4f6778ee1f8952a22a168fb09ec1
s33721 <s33721@pjwstk.edu.pl>
2025-04-11 16:41:54 +0200

    Created a constructor

--- src/LibraryMedia.java
+++ src/LibraryMedia.java
@@ -1,2 +1,11 @@
 public class LibraryMedia {
+    String title;
+    int publicationYear;
+    boolean available;
+
+    public LibraryMedia(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+        this.available = true;
+    }
 }

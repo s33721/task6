6c671dae1283c441cf0a848fbce40b26b14cf497
s33721 <s33721@pjwstk.edu.pl>
2025-04-11 17:21:50 +0200

    Added fields and constructor to the Book class

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,10 @@
-public class Book extends LibraryMedia{
+public class Book extends LibraryMedia {
+    String author;
+    int numberOfPages;
+
+    public Book(String title, int publicationYear, String author, int numberOfPages) {
+        super(title, publicationYear);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+    }
 }

dce296650b36af91f2c52ab29a91d6db47bfccfc
s33721 <s33721@pjwstk.edu.pl>
2025-04-12 22:36:34 +0200

    Added the override displayInformation method and checkDuration method in the Movie class

--- src/Movie.java
+++ src/Movie.java
@@ -1,3 +1,23 @@
 public class Movie extends LibraryMedia{
+    private String director;
+    private int duration;
+    public Movie(String title, int publicationYear, String director, int duration) {
+        super(title, publicationYear);
+        this.director = director;
+        this.duration = duration;
+    }
+    @Override
+    public void displayInformation() {
+        super.displayInformation();
+        System.out.println("Director: " + director);
+        System.out.println("Duration: " + duration);
+    }
+    public void checkDuration() {
+        if (duration > 120){
+            System.out.println("This movie is long.");
+        } else {
+            System.out.println("This movie is not standard.");
+        }
+    }
 
 }

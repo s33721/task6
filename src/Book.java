34c7fd87cbc3a6488be951e9dd786c266443d2fa
s33721 <s33721@pjwstk.edu.pl>
2025-04-12 22:27:43 +0200

    Added the override displayInformation method and checkNumberOfPages method in the Book class

--- src/Book.java
+++ src/Book.java
@@ -7,4 +7,20 @@ public class Book extends LibraryMedia {
         this.author = author;
         this.numberOfPages = numberOfPages;
     }
+
+    @Override
+    public void displayInformation() {
+        super.displayInformation();
+        System.out.println("Author: " + author);
+        System.out.println("Number of pages: " + numberOfPages);
+    }
+
+    public void checkNumberOfPages() {
+        if (this.numberOfPages > 500) {
+            System.out.println("This book is a long one");
+        } else {
+            System.out.println("This book is a standard one");
+        }
+    }
+
 }
